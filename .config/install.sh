890909208fe74299469183b91ccf4a163985a3ef
3m4r5 <63432932+3m4r5@users.noreply.github.com>
2024-09-20 12:27:39 +0300

    Update install.sh

--- .config/install.sh
+++ .config/install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# tested on fedora sway (WIP)
-
 # fix timezone:
 sudo timedatectl set-timezone Asia/Amman
 
@@ -12,9 +10,14 @@ sudo dnf -y install \
 sudo dnf -y install \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+sudo dnf -y copr enable solopasha/hyprland
+
 # list installed packages: dnf repoquery --userinstalled
-#        for yazi and vscode  V                 for junction and Zen V            for yazi V             V for btop
+#                 for vscode  V                 for junction and Zen V            for yazi V             V for btop
 sudo dnf install -y hyprland git micro btop polkit-gnome rofimoji flatpak flameshot ffmpegthumbnailer rocm-smi waybar kitty network-manager-applet blueman rofi-wayland
+
+mkdir ~/.config
+mkdir ~/.local/bin
 
 # yazi:
 wget https://github.com/sxyazi/yazi/releases/latest/download/yazi-x86_64-unknown-linux-gnu.zip
@@ -38,12 +41,14 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 flatpak -y install flathub io.github.zen_browser.zen
 flatpak -y install flathub re.sonny.Junction
 xdg-settings set default-web-browser re.sonny.Junction.desktop
+xdg-mime default re.sonny.Junction.desktop x-scheme-handler/file
+xdg-mime default re.sonny.Junction.desktop inode/directory
 
 
 # Apple Color Emoji: https://gist.github.com/win0err/9d8c7f0feabdfe8a4c9787b02c79ac51
 mkdir ~/.local/share/fonts/
 wget https://github.com/samuelngs/apple-emoji-linux/releases/latest/download/AppleColorEmoji.ttf -O ~/.local/share/fonts/AppleColorEmoji.ttf
-sudo sed -i -e 's/Noto Color/Apple Color Emoji<\/family><family>Noto Color/g' /etc/fonts/conf.d/60-generic.conf
+sudo sed -i -e 's/Noto Color/Apple Color Emoji<\/family><family>Noto Color/g' /etc/fonts/conf.d/60-generic.conf # TODO 45
 mkdir ~/.config/fontconfig/
 tee ~/.config/fontconfig/fonts.conf << FONTS
 <?xml version="1.0" encoding="UTF-8"?>
@@ -75,15 +80,21 @@ tee ~/.config/fontconfig/fonts.conf << FONTS
 FONTS
 
 # nerd fonts:
+cd ~/.local/share/fonts/
 wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/NerdFontsSymbolsOnly.zip
 unzip NerdFontsSymbolsOnly.zip
 rm -f NerdFontsSymbolsOnly.zip LICENSE README.md
-sudo mkdir /usr/share/fonts/NerdFontsSymbolsOnly/
-sudo mv SymbolsNerdFont-Regular.ttf SymbolsNerdFontMono-Regular.ttf /usr/share/fonts/NerdFontsSymbolsOnly/
-rm -rf ~/.cache/fontconfig
 
+rm -rf ~/.cache/fontconfig
 sudo fc-cache -r -v
 fc-cache -f -v
+
+cd ~
+echo "
+if [ -f ~/.config/.bash_aliases ]; then
+. ~/.config/.bash_aliases
+fi
+" >> .bashrc
 
 : '
 todo:
@@ -123,5 +134,3 @@ other:
 - manage with gnu stow / dotter & upload to github
 - install script
 '
-
-reboot
